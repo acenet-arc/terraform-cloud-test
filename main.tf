5d56576da5158870a69ecc8d3d5b6d73cf925da6
Chris Geroux <chris.m.geroux@gmail.com>
2023-04-13 16:03:27 -0300

    try adding a new node

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ module "openstack" {
   instances = {
     mgmt   = { type = "p4-6gb", tags = ["puppet", "mgmt", "nfs"], count = 1 }
     login  = { type = "p2-3gb", tags = ["login", "public", "proxy"], count = 1 }
-    node   = { type = "p2-3gb", tags = ["node"], count = 1 }
+    node   = { type = "p2-3gb", tags = ["node"], count = 2 }
   }
 
   # var.pool is managed by Slurm through Terraform REST API.
